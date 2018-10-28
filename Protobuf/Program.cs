416891e31282f487fcfe40a610b4f2ef6f5b9d22
Liviu Şoşu <sosuliviu@gmail.com>
2018-10-28 20:03:05 +0200

    Basic Protocol buffer added

---
+++ Protobuf/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf;
+
+namespace Protobuf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tutorial objTutorial = new Tutorial();
+            objTutorial.Author = "Author";
+            objTutorial.Name = "Name 1";
+            objTutorial.PageCount = 50;
+            byte[] tempByte = GPBSerialization(objTutorial);
+            Tutorial objReconstructed = (Tutorial)GDPDeserialization(tempByte);
+        }
+
+        public static byte[] GPBSerialization(object objectToSerialize)
+        {
+            if (objectToSerialize == null)
+            {
+                return null;
+            }
+            try
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    Serializer.Serialize(stream, objectToSerialize);
+                    return stream.ToArray();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static object GDPDeserialization(byte[] data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+            try
+            {
+                using (MemoryStream stream = new MemoryStream(data))
+                {
+                    return Serializer.Deserialize(typeof(Tutorial), stream);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Protobuf/Tutorial.cs
@@ -0,0 +1,20 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protobuf
+{
+    [ProtoContract]
+    public class Tutorial
+    {
+        [ProtoMember(1)]
+        public string Author { get; set; }
+        [ProtoMember(2)]
+        public string Name { get; set; }
+        [ProtoMember(3)]
+        public int PageCount { get; set; }
+    }
+}
