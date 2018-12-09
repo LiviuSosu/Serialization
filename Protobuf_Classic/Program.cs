dd3c4944a588f253abf314e638612a2aeeca1708
Liviu Şoşu <sosuliviu@gmail.com>
2018-12-09 11:14:25 +0200

    added classic serializer to protobuf

--- Protobuf_Classic/Program.cs
+++ Protobuf_Classic/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Google.Protobuf;
+using Google.Protobuf.Examples.AddressBook;
+using System;
+using System.Diagnostics;
+using System.IO;
+using static Google.Protobuf.Examples.AddressBook.Books.Types;
+using static Google.Protobuf.Examples.AddressBook.Books.Types.BookInfo.Types;
 
 namespace Protobuf_Classic
 {
@@ -6,6 +12,31 @@ namespace Protobuf_Classic
     {
         static void Main(string[] args)
         {
+            BookInfo bookInfo;
+            Books books = new Books();
+            for (int i = 1; i <= Config.Config.DataSetSize; i++)
+            {
+                bookInfo = new BookInfo();
+                bookInfo.Id = i;
+                bookInfo.Author = new Author() { Name = "Author name " + i };
+                bookInfo.Title = "Title " + i;
+                bookInfo.PageCount = i;
+
+                books.Books_.Add(bookInfo);
+            }
+            var sw = new Stopwatch();
+            sw.Start();
+            using (var output = File.Create("Data.dat"))
+            {
+                books.WriteTo(output);
+            }
+
+            using (var input = File.OpenRead("Data.dat"))
+            {
+                books = Books.Parser.ParseFrom(input);
+            }
+            sw.Stop();
+            long elapsedMilliseconds = sw.ElapsedMilliseconds;
             Console.WriteLine("Hello World!");
         }
     }
